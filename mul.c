6b3bcfbd20f8d211f9c95dc8df5d3e931711f47a
HopeSamuel <mariushope007>
2023-01-22 12:59:13 -0800

    m

--- mul.c
+++ mul.c
@@ -5,7 +5,7 @@
   * @counter: line_number
   * Return: no return
   */
-void f_mul(stack_t ** head , unsigned int counter)
+void f_mul(stack_t **head,unsigned int counter)
 {
 	stack_t * h;
 	int len = 0, aux;
