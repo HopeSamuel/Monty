67d4ba0228068c6bcce87d91fb62f72a9ed0ab62
HopeSamuel <mariushope007>
2023-01-22 10:21:51 -0800

    mt

--- monty.h
+++ monty.h
@@ -8,10 +8,13 @@
 #include <string.h>
 #include <ctype.h>
  /**
-  * struct stack_s - doubly linked list representation of a stack (or queue)
+  * struct stack_s - doubly linked list repre
+sentation of a stack (or queue)
   * @n: integer
-  * @prev: points to the previous element of the stack (or queue)
-  * @next: points to the next element of the stack (or queue)
+  * @prev: points to the previous element of the 
+stack (or queue)
+  * @next: points to the next element of the 
+stack (or queue)
   * Description: doubly linked list node structure
   * for stack, queues, LIFO, FIFO Holberton project
   */
