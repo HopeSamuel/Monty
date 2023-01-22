bdd65961f65b1ceac24d83c6876a4a1ddb12d760
HopeSamuel <mariushope007>
2023-01-22 12:07:06 -0800

    mt

--- monty.h
+++ monty.h
@@ -34,9 +34,9 @@ typedef struct stack_s
   */
 typedef struct bus_s
 {
-	char *arg;
-	FILE *file;
-	char *content;
+	char * arg;
+	FILE * file;
+	char * content;
 	int lifi;
 }  bus_t;
 extern bus_t bus;
@@ -49,17 +49,20 @@ extern bus_t bus;
    */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+	char * opcode;
+	void (*f)(stack_t **stack, unsigned int 
+line_number);
 } instruction_t;
 
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+char *_realloc(char *ptr, unsigned int old_size, 
+unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
 void f_push(stack_t **head, unsigned int number);
 void f_pall(stack_t **head, unsigned int number);
 void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
+int execute(char *content, stack_t **head, 
+		unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
