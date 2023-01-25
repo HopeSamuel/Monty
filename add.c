5afe99ea77c78241a7a61cd830de49519cb604ea
Kornebari <tambarikornebari@gmail.com>
2023-01-25 09:51:49 -0800

    add.c

--- add.c
+++ add.c
@@ -8,7 +8,8 @@
 void f_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
-	int len =0, aux;
+	int len = 0, aux;
+
 	h - *head;
 	while (h)
 	{
@@ -25,7 +26,7 @@ void f_add(stack_t **head, unsigned int counter)
 	}
 	h = *head;
 	aux = h->n + h->next->n;
-	h-next->n = aux;
+	h - next->n = aux;
 	*head = h->next;
 	frree(h);
 }
---
+++ mod.c
@@ -0,0 +1,4 @@
+#include "monty.h"
+/**
+ * f_mod - computes the rest of the division of the second
+ * top element of the stack
