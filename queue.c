6a208c97f35a02089cb0d89b751a7a27e805b0e7
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-03-02 17:26:06 +0100

    M

---
+++ queue.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_queue - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
