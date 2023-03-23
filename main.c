f08399a3b4f754fa39feee90dabff84b6a696d38
HopeSamuel <113640033+HopeSamuel@users.noreply.github.com>
2023-03-23 14:23:08 +0100

    M

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 	unsigned int counter = 0;
 
-        if (argc != 2)
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
